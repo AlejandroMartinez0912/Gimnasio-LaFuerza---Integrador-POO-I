027a5e501841fbfb514679c21c36ae661a94354b
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-02-10 09:24:59 -0300

    cambié que la clase ejercicio pueda tener muchos grupos musculares

--- lafuerza/src/main/java/com/lafuerzaapp/modelo/Ejercicio.java
+++ lafuerza/src/main/java/com/lafuerzaapp/modelo/Ejercicio.java
@@ -1,19 +1,21 @@
 package com.lafuerzaapp.modelo;
 
+import java.util.Set;
+
 public class Ejercicio {
     private int idEjercicio;
     private String nombre;
     private String descripcion;
-    private GrupoMuscular grupoMuscular;
+    private Set<GrupoMuscular> gruposMusculares;
 
     public Ejercicio() {
     }
 
-    public Ejercicio(int idEjercicio, String nombre, String descripcion, GrupoMuscular grupoMuscular) {
+    public Ejercicio(int idEjercicio, String nombre, String descripcion, Set<GrupoMuscular> gruposMusculares) {
         this.idEjercicio = idEjercicio;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.grupoMuscular = grupoMuscular;
+        this.gruposMusculares = gruposMusculares;
     }
 
     public int getIdEjercicio() {
@@ -40,11 +42,13 @@ public class Ejercicio {
         this.descripcion = descripcion;
     }
 
-    public GrupoMuscular getGrupoMuscular() {
-        return grupoMuscular;
+    public Set<GrupoMuscular> getGruposMusculares() {
+        return gruposMusculares;
     }
 
-    public void setGrupoMuscular(GrupoMuscular grupoMuscular) {
-        this.grupoMuscular = grupoMuscular;
+    public void setGruposMusculares(Set<GrupoMuscular> gruposMusculares) {
+        this.gruposMusculares = gruposMusculares;
     }
+
+    
 }
