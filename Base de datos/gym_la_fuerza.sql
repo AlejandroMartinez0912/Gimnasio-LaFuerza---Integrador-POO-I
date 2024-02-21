2695e888eb638f81e07542d2291a9f9814351bbb
Marcos <marcosnatad@gmail.com>
2024-02-21 18:41:35 -0300

    Creación de la base de datos

---
+++ Base de datos/gym_la_fuerza.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-02-2024 a las 22:38:04
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gym_la_fuerza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `sexo` varchar(10) DEFAULT NULL,
+  `fechaIngreso` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_entrenamiento_liente`
+--
+
+CREATE TABLE `detalle_entrenamiento_liente` (
+  `id` int(11) NOT NULL,
+  `entrenamientoRealizado` text DEFAULT NULL,
+  `series` int(11) DEFAULT NULL,
+  `repeticiones` int(11) DEFAULT NULL,
+  `peso` double DEFAULT NULL,
+  `volumenEntrenamiento` int(11) DEFAULT NULL,
+  `entrenamiento_cliente_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ejercicio`
+--
+
+CREATE TABLE `ejercicio` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `grupo_muscular_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrenamiento`
+--
+
+CREATE TABLE `entrenamiento` (
+  `id` int(11) NOT NULL,
+  `idEjercicio` int(11) DEFAULT NULL,
+  `series` int(11) DEFAULT NULL,
+  `repeticiones` int(11) DEFAULT NULL,
+  `descanso` int(11) DEFAULT NULL,
+  `duracionMinutos` int(11) DEFAULT NULL,
+  `ejercicio_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrenamiento_cliente`
+--
+
+CREATE TABLE `entrenamiento_cliente` (
+  `id` int(11) NOT NULL,
+  `idCliente` int(11) DEFAULT NULL,
+  `duracionSemanas` int(11) DEFAULT NULL,
+  `fechaInicio` date DEFAULT NULL,
+  `idTutor` int(11) DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT NULL,
+  `volumenSemanal` int(11) DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `tutor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo_muscular`
+--
+
+CREATE TABLE `grupo_muscular` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tutor`
+--
+
+CREATE TABLE `tutor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle_entrenamiento_liente`
+--
+ALTER TABLE `detalle_entrenamiento_liente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `entrenamiento_cliente_id` (`entrenamiento_cliente_id`);
+
+--
+-- Indices de la tabla `ejercicio`
+--
+ALTER TABLE `ejercicio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `grupo_muscular_id` (`grupo_muscular_id`);
+
+--
+-- Indices de la tabla `entrenamiento`
+--
+ALTER TABLE `entrenamiento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ejercicio_id` (`ejercicio_id`);
+
+--
+-- Indices de la tabla `entrenamiento_cliente`
+--
+ALTER TABLE `entrenamiento_cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cliente_id` (`cliente_id`),
+  ADD KEY `tutor_id` (`tutor_id`);
+
+--
+-- Indices de la tabla `grupo_muscular`
+--
+ALTER TABLE `grupo_muscular`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_entrenamiento_liente`
+--
+ALTER TABLE `detalle_entrenamiento_liente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ejercicio`
+--
+ALTER TABLE `ejercicio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `entrenamiento`
+--
+ALTER TABLE `entrenamiento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `entrenamiento_cliente`
+--
+ALTER TABLE `entrenamiento_cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupo_muscular`
+--
+ALTER TABLE `grupo_muscular`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_entrenamiento_liente`
+--
+ALTER TABLE `detalle_entrenamiento_liente`
+  ADD CONSTRAINT `detalle_entrenamiento_liente_ibfk_1` FOREIGN KEY (`entrenamiento_cliente_id`) REFERENCES `entrenamiento_cliente` (`id`);
+
+--
+-- Filtros para la tabla `ejercicio`
+--
+ALTER TABLE `ejercicio`
+  ADD CONSTRAINT `ejercicio_ibfk_1` FOREIGN KEY (`grupo_muscular_id`) REFERENCES `grupo_muscular` (`id`);
+
+--
+-- Filtros para la tabla `entrenamiento`
+--
+ALTER TABLE `entrenamiento`
+  ADD CONSTRAINT `entrenamiento_ibfk_1` FOREIGN KEY (`ejercicio_id`) REFERENCES `ejercicio` (`id`);
+
+--
+-- Filtros para la tabla `entrenamiento_cliente`
+--
+ALTER TABLE `entrenamiento_cliente`
+  ADD CONSTRAINT `entrenamiento_cliente_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `entrenamiento_cliente_ibfk_2` FOREIGN KEY (`tutor_id`) REFERENCES `tutor` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
