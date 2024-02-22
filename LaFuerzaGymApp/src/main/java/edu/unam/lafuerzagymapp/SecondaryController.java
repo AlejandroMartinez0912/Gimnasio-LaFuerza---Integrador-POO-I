55586cecd4732791f6717f212c729faac3553f42
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-02-22 18:25:55 -0300

    Mudanza a Netbeans

---
+++ LaFuerzaGymApp/src/main/java/edu/unam/lafuerzagymapp/SecondaryController.java
@@ -0,0 +1,12 @@
+package edu.unam.lafuerzagymapp;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
