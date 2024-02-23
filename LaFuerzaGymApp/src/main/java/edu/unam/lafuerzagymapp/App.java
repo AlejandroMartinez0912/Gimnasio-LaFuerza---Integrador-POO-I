5191aa80013375fe56c645bb3176706b44e716fd
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-02-23 15:49:45 -0300

    Corregí la ejecución de vistas y agregué su fondo a cada una

--- LaFuerzaGymApp/src/main/java/edu/unam/lafuerzagymapp/App.java
+++ LaFuerzaGymApp/src/main/java/edu/unam/lafuerzagymapp/App.java
@@ -17,17 +17,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("homeView"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/edu/unam/lafuerzagymapp/vistas/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
