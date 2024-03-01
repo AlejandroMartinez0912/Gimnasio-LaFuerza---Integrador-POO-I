c7d7294fb44bea5757873786b563b361f237e835
Alejandro Martinez <113720396+AlejandroMartinez0912@users.noreply.github.com>
2024-03-01 19:44:52 -0300

    Modificación de vistas

--- LaFuerzaGym/src/edu/unam/vistas/addNuevoEntrenamientoController.java
+++ LaFuerzaGym/src/edu/unam/vistas/addNuevoEntrenamientoController.java
@@ -12,60 +12,61 @@ import javafx.scene.control.TextField;
 
 public class addNuevoEntrenamientoController {
     @FXML
+    private Label labelTituloApp;
+
+    @FXML
+    private Label labelTítuloVista;
+
+    @FXML
+    private ComboBox<String> comboBoxEjercicio;
+
+    @FXML
+    private Label labelEjercicio;
+
+    @FXML
+    private TextField txtNombreSeries;
+
+    @FXML
+    private Label labelSeries;
+
+    @FXML
+    private TextField txtRepeticiones;
+
+    @FXML
+    private Label labelRepeticiones;
+
+    @FXML
+    private TextField txtDescanso;
+
+    @FXML
+    private Label labelDescanso;
+
+    @FXML
     private Button btnGuardarNuevoEjercicio;
 
     @FXML
     private Button btnVolverHome;
 
     @FXML
-    private ComboBox<?> comboBoxGrupoMuscular;
-
-    @FXML
-    private Label labelTítuloVista;
-
-    @FXML
-    private Label labelTituloApp;
-
-    @FXML
-    private Label labelNombreEjercicio;
-
-    @FXML
-    private Label lblObligatorio;
-
-    @FXML
-    private Label labelGrupoMuscular;
-
-    @FXML
-    private Label lblObligatorio1;
-
-    @FXML
-    private TextField txtNombreEjercicio;
-
-    @FXML
-    private TextField txtNombreEjercicio1;
-
-    @FXML
-    private Label labelGrupoMuscular1;
-
-    @FXML
-    private Label lblObligatorio11;
-
-    @FXML
-    private TextField txtNombreEjercicio2;
-
-    @FXML
-    private Label labelGrupoMuscular2;
-
-    @FXML
-    private Label lblObligatorio12;
-
-    @FXML
     void guardarNuevoEjercicio() throws IOException {
         // Implementa la lógica para guardar un nuevo ejercicio
+        String ejercicioSeleccionado = comboBoxEjercicio.getValue();
+        String nombreSeries = txtNombreSeries.getText();
+        String repeticiones = txtRepeticiones.getText();
+        String descanso = txtDescanso.getText();
+
+        //Conexion
+
+        //Alerta
+        //Limpiar campos
+        comboBoxEjercicio.setValue(null);
+        txtNombreSeries.clear();
+        txtDescanso.clear();
+        txtRepeticiones.clear();
     }
 
     @FXML
     void volverHome() throws IOException {
-        App.setRoot("homeView");
+        
     }
 }
--- LaFuerzaGym/src/edu/unam/vistas/viewEjerciciosController.java
+++ LaFuerzaGym/src/edu/unam/vistas/viewEjerciciosController.java
@@ -3,34 +3,60 @@ package edu.unam.vistas;
 import java.io.IOException;
 
 import edu.unam.App;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class viewEjerciciosController {
-     @FXML
-    private Button btnGuardarNuevoCliente;
+
+    @FXML
+    private Button btnGuardarNuevoEjercicio;
 
     @FXML
     private Button btnVolverHome;
 
     @FXML
-    private Label labelTituloApp;
+    private ComboBox<?> comboBoxGrupoMuscular;
+
+    @FXML
+    private Label labelGrupoMuscular;
+
+    @FXML
+    private Label labelNombreEjercicio;
 
     @FXML
     private Label labelTítuloVista;
 
     @FXML
-    private TableColumn<?, ?> tableviewEjercicios;
+    private TableColumn<?, ?> tablaGrupoMuscular;
 
     @FXML
-    void guardarNuevoCliente(ActionEvent event) {
+    private TableColumn<?, ?> tablaeditarEjercicio;
+
+    @FXML
+    private TableColumn<?, ?> tablaeliminarEjercicio;
+
+    @FXML
+    private TableColumn<?, ?> tableEjercicios;
+
+    @FXML
+    private TableView<?> tableViewEjercicios;
+
+    @FXML
+    private TextField txtNombreEjercicio;
+
+    @FXML
+    void guardarNuevoEjercicio(ActionEvent event) {
 
     }
+
     @FXML
-    void volverHome() throws IOException{
+    void volverHome()throws IOException {
         App.setRoot("homeView");
     }
 
