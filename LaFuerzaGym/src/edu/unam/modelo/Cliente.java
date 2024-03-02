9412d24c3b4e11529577c8a44fcc9b5f391b6684
Marcos2497 <marcosnatad@gmail.com>
2024-03-02 14:06:35 -0300

    Se agrego JPA a la clase Cliente y se creo ServicioClientne.

--- LaFuerzaGym/src/edu/unam/modelo/Cliente.java
+++ LaFuerzaGym/src/edu/unam/modelo/Cliente.java
@@ -2,24 +2,47 @@ package edu.unam.modelo;
 
 import java.util.Date;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
+@Table(name = "cliente")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_cliente", nullable = false)
     private int idCliente;
+    @Column(name = "nombre", nullable = false, length = 100)
+    @Basic
     private String nombre;
+    @Column(name = "apellido", nullable = false, length = 100)
+    @Basic
     private String apellido;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_nacimiento", nullable = false)
     private Date fechaNacimiento;
+    @Column(name = "sexo", nullable = false, length = 1)
+    @Basic
     private String sexo;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_ingreso", nullable = false)
     private Date fechaIngreso;
-
+    
     public Cliente() {
     }
 
-    public Cliente(String nombre, String apellido) {
+    /*public Cliente(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
-    }
+    } */
+    
     public Cliente(int idCliente, String nombre, String apellido, Date fechaNacimiento, String sexo, Date fechaIngreso) {
         this.idCliente = idCliente;
         this.nombre = nombre;
