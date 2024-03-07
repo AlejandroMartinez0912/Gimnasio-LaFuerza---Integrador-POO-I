20fef66c6045130de16ff3cc214244beae69eab5
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-03-07 20:25:07 -0300

    Vista de DetalleEntrenamientoCliente

--- LaFuerzaGym/src/edu/unam/vistas/viewDetalleEntrenamientoClienteController.java
+++ LaFuerzaGym/src/edu/unam/vistas/viewDetalleEntrenamientoClienteController.java
@@ -1,5 +1,82 @@
 package edu.unam.vistas;
 
+import java.io.IOException;
+
+import edu.unam.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+
 public class viewDetalleEntrenamientoClienteController {
-    
+
+    @FXML
+    private Button btnEditarDetalleEntrenamiento;
+
+    @FXML
+    private Button btnEliminarDetalleEntrenamiento;
+
+    @FXML
+    private Button btnGuardarNuevoDetalleEntrenamiento;
+
+    @FXML
+    private Button btnVolverHome;
+
+    @FXML
+    private TableColumn<?, ?> clienteColumn;
+
+    @FXML
+    private ComboBox<?> comboBoxEjercicio;
+
+    @FXML
+    private ComboBox<?> comboBoxRepeticiones;
+
+    @FXML
+    private ComboBox<?> comboBoxSeries;
+
+    @FXML
+    private TableColumn<?, ?> ejercicioColumn;
+
+    @FXML
+    private TableColumn<?, ?> grupoMuscularColumn;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private Label labelEjercicio;
+
+    @FXML
+    private Label labelRepeticiones;
+
+    @FXML
+    private Label labelSeries;
+
+    @FXML
+    private Label labelTítuloVista;
+
+    @FXML
+    private TableColumn<?, ?> repeticionesColumn;
+
+    @FXML
+    private TableColumn<?, ?> seriesColumn;
+
+    @FXML
+    private TableView<?> tableDetalleEntrenamiento;
+
+    @FXML
+    void guardarNuevoEntrenamiento(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void volverHome(ActionEvent event) throws IOException {
+        App.setRoot("homeView");     
+    }
+
 }
