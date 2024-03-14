cca62061cf4d0747d462afe24c6d649ed79ad9ea
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-03-14 10:55:47 -0300

    Registro de detalle de rutina

--- LaFuerzaGym/src/edu/unam/vistas/viewDetalleRutinaController.java
+++ LaFuerzaGym/src/edu/unam/vistas/viewDetalleRutinaController.java
@@ -2,21 +2,22 @@ package edu.unam.vistas;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import edu.unam.App;
+import edu.unam.modelo.DetalleRutina;
 import edu.unam.modelo.EntrenamientoCliente;
 import edu.unam.modelo.Rutina;
 import edu.unam.repositorio.Repositorio;
 import edu.unam.servicios.ServicioCliente;
+import edu.unam.servicios.ServicioDetalleRutina;
 import edu.unam.servicios.ServicioEntrenamientoCliente;
 import edu.unam.servicios.ServicioRutina;
-import edu.unam.servicios.ServicioTutor;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -28,13 +29,18 @@ public class viewDetalleRutinaController {
     //Declaración del repositorio
     private EntityManagerFactory emf;
     private Repositorio repositorio;
+    private ServicioDetalleRutina servicioDetalleRutina;
+    @SuppressWarnings("unused")
     private ServicioEntrenamientoCliente servicioEntrenamientoCliente;
+    @SuppressWarnings("unused")
     private ServicioRutina servicioRutina;
+    @SuppressWarnings("unused")
     private ServicioCliente servicioCliente;
 
     public viewDetalleRutinaController() {
         emf = Persistence.createEntityManagerFactory("LaFuerzaPU");
         repositorio = new Repositorio(emf);
+        servicioDetalleRutina = new ServicioDetalleRutina(repositorio);
         servicioEntrenamientoCliente = new ServicioEntrenamientoCliente(repositorio);
         servicioRutina = new ServicioRutina(repositorio);
         servicioCliente = new ServicioCliente(repositorio);
@@ -140,6 +146,48 @@ public class viewDetalleRutinaController {
     @FXML
     void guardarNuevoEntrenamiento(ActionEvent event) throws IOException {
 
+        Alert alertSuccess = new Alert(Alert.AlertType.INFORMATION);
+        alertSuccess.setTitle("Error");
+        alertSuccess.setHeaderText(null);
+        alertSuccess.setContentText("El detalle de la rutina del cliente se agregó correctamente.");
+
+
+        Alert alertError = new Alert(Alert.AlertType.ERROR);
+        alertError.setTitle("Error");
+        alertError.setHeaderText(null);
+        alertError.setContentText("Error al guardar el detalle de la rutina del cliente.");
+
+        //Obtenemos los valores de los campos
+        String nombreEjercicio = comboBoxEjercicio.getValue();
+        Integer series = comboBoxSeries.getValue();
+        Integer repeticiones = comboBoxRepeticiones.getValue();
+        Double peso = Double.parseDouble(txtPeso.getText());
+
+        //Realizamos la validación de los campos
+        if(nombreEjercicio == null || series == null || repeticiones == null || peso == null){
+            alertError.setContentText("Todos los campos son obligatorios.");
+            alertError.showAndWait();
+            return;
+        }
+
+        try {
+            DetalleRutina detalleRutina = new DetalleRutina();
+            detalleRutina.setEntrenamientoCliente(entrenamientoCliente);
+            detalleRutina.setSeries(series);
+            detalleRutina.setRepeticiones(repeticiones);
+            detalleRutina.setPeso(peso);
+            detalleRutina.setVolumenRutina(series * repeticiones * peso);
+
+            //Guardamos el detalle de la rutina
+            servicioDetalleRutina.agregarDetalleRutina(detalleRutina);
+
+            //Mostramos mensaje de éxito
+            alertSuccess.showAndWait();
+
+        } catch (Exception e) {
+            alertError.showAndWait();
+        }
+        
     }
 
     @FXML
